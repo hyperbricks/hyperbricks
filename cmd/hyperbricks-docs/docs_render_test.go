faafd3face22cb2b48b11384bf73c5c9ba07d67d
hyperbricks <hyperbricks@robbertstreng.nl>
2025-01-13 21:23:48 +0100

    making some progress

--- cmd/hyperbricks-docs/docs_render_test.go
+++ cmd/hyperbricks-docs/docs_render_test.go
@@ -144,8 +144,6 @@ func Test_TestAndDocumentationRender(t *testing.T) {
 		val := reflect.ValueOf(cfg.Config)
 		fmt.Print(processFieldsWithSquash(val, cfg))
 
-		//fmt.Print(processNonEmbeddedFields(val, cfg))
-
 		// Iterate through embedded fields
 		for embeddedName, fieldTag := range cfg.Embedded {
 			fmt.Printf("\nEmbedded Field: %s (mapped as: %s)\n", embeddedName, fieldTag)
@@ -158,107 +156,6 @@ func Test_TestAndDocumentationRender(t *testing.T) {
 			}
 		}
 	}
-}
-
-func processNonEmbeddedFields(val reflect.Value, cfg DocumentationTypeStruct) string {
-	var out strings.Builder
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	if val.Kind() != reflect.Struct {
-		fmt.Println("Not a struct, skipping.")
-		return ""
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
-		if field.Anonymous {
-			continue // Skip embedded fields
-		}
-		tag := field.Tag
-
-		if tag.Get("mapstructure") != "" {
-			out.WriteString(fmt.Sprintf("Field: %s ->%s\n", tag.Get("mapstructure"), field.Name))
-			out.WriteString(fmt.Sprintf("description: %s\n", field.Tag.Get("description")))
-			if field.Tag.Get("example") != "" {
-				example := _checkAndReadFile(field.Tag.Get("example"))
-				out.WriteString(fmt.Sprintf("example: %s\n", example))
-			} else if field.Tag.Get("mapstructure") != "" {
-				file := strings.ToLower(cfg.Name) + "-" + tag.Get("mapstructure")
-				example := _checkAndReadFile("{!{" + file + "}}")
-				out.WriteString(fmt.Sprintf("example: %s\n", example))
-			}
-		}
-		//
-		//out.WriteString(fmt.Sprintf("File: %s\n", file))
-		//out.WriteString(fmt.Sprintf("\tmapstructure: %s\n", tag))
-		//out.WriteString(fmt.Sprintf("\tdescription: %s\n", field.Tag.Get("description")))
-		//out.WriteString(fmt.Sprintf("\texample: %s\n", field.Tag.Get("example")))
-
-	}
-	return out.String()
-}
-
-func processFields(val reflect.Value, cfg DocumentationTypeStruct) string {
-
-	var out strings.Builder
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	if val.Kind() != reflect.Struct {
-		fmt.Println("Not a struct, skipping.")
-		return ""
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
-		tag := field.Tag
-
-		if tag.Get("mapstructure") != "" {
-			out.WriteString(fmt.Sprintf("Field: %s ->%s\n", tag.Get("mapstructure"), field.Name))
-			out.WriteString(fmt.Sprintf("description: %s\n", field.Tag.Get("description")))
-			if field.Tag.Get("example") != "" {
-				example := _checkAndReadFile(field.Tag.Get("example"))
-				out.WriteString(fmt.Sprintf("example: %s\n", example))
-			} else if field.Tag.Get("mapstructure") != "" {
-				file := strings.ToLower(cfg.Name) + "-" + tag.Get("mapstructure")
-				example := _checkAndReadFile("{!{" + file + "}}")
-				out.WriteString(fmt.Sprintf("example: %s\n", example))
-			}
-		}
-
-		//file := strings.ToLower(cfg.Name) + "-" + tag.Get("mapstructure")
-		//out.WriteString(fmt.Sprintf("File: %s\n", file))
-		//out.WriteString(fmt.Sprintf("\tmapstructure: %s\n", tag))
-		//out.WriteString(fmt.Sprintf("\tdescription: %s\n", field.Tag.Get("description")))
-		//out.WriteString(fmt.Sprintf("\texample: %s\n", field.Tag.Get("example")))
-
-	}
-	return out.String()
-}
-
-func findFieldByName(val reflect.Value, fieldName string) reflect.Value {
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	if !val.IsValid() || val.Kind() != reflect.Struct {
-		return reflect.Value{}
-	}
-
-	fieldVal := val.FieldByName(fieldName)
-	if fieldVal.IsValid() {
-		return fieldVal
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		subVal := val.Field(i)
-		found := findFieldByName(subVal, fieldName)
-		if found.IsValid() {
-			return found
-		}
-	}
-
-	return reflect.Value{}
 }
 
 func processFieldsWithSquash(val reflect.Value, cfg DocumentationTypeStruct) string {
@@ -291,14 +188,12 @@ func processFieldsWithSquash(val reflect.Value, cfg DocumentationTypeStruct) str
 				file := strings.ToLower(cfg.Name) + "-" + tag
 				example := _checkAndReadFile("{!{" + file + "}}")
 				out.WriteString(fmt.Sprintf("example: %s\n", example))
+
+				// PARSE HYPERSCRIPT
+				// RUN THE TEST (compare json with serialized output go object)
+
 			}
 		}
-
-		//file := strings.ToLower(cfg.Name) + "-" + tag
-		//out.WriteString(fmt.Sprintf("File: %s\n", file))
-		//out.WriteString(fmt.Sprintf("\tmapstructure: %s\n", tag))
-		//out.WriteString(fmt.Sprintf("\tdescription: %s\n", field.Tag.Get("description")))
-		//out.WriteString(fmt.Sprintf("\texample: %s\n", field.Tag.Get("example")))
 
 	}
 	return out.String()
@@ -348,4 +243,28 @@ func _checkAndReadFile(input string) string {
 	}
 
 	return input
+}
+
+func findFieldByName(val reflect.Value, fieldName string) reflect.Value {
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	if !val.IsValid() || val.Kind() != reflect.Struct {
+		return reflect.Value{}
+	}
+
+	fieldVal := val.FieldByName(fieldName)
+	if fieldVal.IsValid() {
+		return fieldVal
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		subVal := val.Field(i)
+		found := findFieldByName(subVal, fieldName)
+		if found.IsValid() {
+			return found
+		}
+	}
+
+	return reflect.Value{}
 }
