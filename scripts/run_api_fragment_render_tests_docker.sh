ee54fea44fc7c2c224197a9b04229f447a0c8367
hyperbricks <hyperbricks@robbertstreng.nl>
2025-08-04 13:05:09 +0200

    more tweaking

--- scripts/run_api_fragment_render_tests_docker.sh
+++ scripts/run_api_fragment_render_tests_docker.sh
@@ -3,7 +3,7 @@
 # Start the Go server in the background
 echo "Starting docker server..."
 go run ./cmd/testing/main.go &
-docker-compose -f ./test/dedicated/docker/docker-compose.yml up -d
+docker compose -f ./test/dedicated/docker/docker-compose.yml up -d
 
 # Give the server a moment to start
 sleep 10
@@ -13,5 +13,5 @@ echo "Running docker API_RENDER and API_FRAGENT_RENDER test..."
 go test -v ./test/dedicated/dedicted_test.go -args -directory="./api-tests/"
 
 # Stop the server after the test
-docker-compose -f ./test/dedicated/docker/docker-compose.yml down -v
+docker compose -f ./test/dedicated/docker/docker-compose.yml down -v
 pkill -f "/exe/main"
